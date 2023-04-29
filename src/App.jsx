787817dc78e499d1f37880a6f225d237a0d25ec6
Pdeepak18 <85224426+Pdeepak18@users.noreply.github.com>
2023-04-29 12:13:15 +0530

    plan-with-love

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import "./App.css";
+import data from "./data"
+import Tours from "./components/Tours";
 
 function App() {
+console.log(data)
+  const [tours,setTours] = useState(data)
+
+  function removeTour(id){
+    const newTours = tours.filter( tour => tour.id !== id)
+    setTours(newTours)
+  }
+// reloading the page
+ 
+
+  if(tours.length === 0){
+    return (
+      <div className=" w-[100vw] h-[100vh] flex justify-center items-center flex-col">
+        <h2 className="text-2xl">No Tours Left</h2>
+        <button onClick={() => setTours(data)} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full'>Refresh</button>
+      </div>
+    )
+  }
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
-    </main>
-  );
+   <div className="">
+      <Tours tours={tours} removeTour={removeTour}/>
+   </div>
+  )
 }
 
 export default App;
